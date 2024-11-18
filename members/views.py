e33b018e91cd80ce5d620b60d5b08d7936198c6b
Greg1400 <herzig.gregoire@gmail.com>
2024-11-18 16:36:08 +0100

    Delete line 1 wich imports render and correct false written template on line 5

--- members/views.py
+++ members/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-    template = loader.get_template('myfrist.html')
+    template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
