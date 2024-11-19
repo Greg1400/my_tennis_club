90f0070419d94295ec328c687e66ee245f585703
Greg1400 <herzig.gregoire@gmail.com>
2024-11-19 11:11:31 +0100

    Miss spelt correction on context

--- members/views.py
+++ members/views.py
@@ -5,7 +5,7 @@ from .models import Member
 def members(request):
     mymembers = Member.objects.all().values()
     template = loader.get_template('all_members.html')
-    conzexz = {
+    context = {
         'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
