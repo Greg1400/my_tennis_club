c0198a5315e3b56ff94d0c0152db9dd248c61f96
Greg1400 <herzig.gregoire@gmail.com>
2024-11-19 10:54:00 +0100

    Miss spelt correction of DateField

--- members/models.py
+++ members/models.py
@@ -4,4 +4,4 @@ class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField()
-    joined_date = models.DateFiel()
+    joined_date = models.DateField()
