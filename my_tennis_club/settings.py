2dd31ca9531d54a9ad338eab5c537bb9ff92ebf0
Greg1400 <herzig.gregoire@gmail.com>
2024-11-19 15:48:02 +0100

    add  error page + put DEBUG on False and ALLOWD_HOSTS to all

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n8=d-=qrf$)f1=1l3-s62nhv1*n3)w(*kttnrk%sn*)78qfde+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
