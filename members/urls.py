c50b58b7ecd153c30ec187f769f395feb11f6031
Greg1400 <herzig.gregoire@gmail.com>
2024-11-19 15:38:51 +0100

    Add comments and root URL for main index

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+"""URL Configruation for the application
+"""
 urlpatterns = [
-    path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'),
+    path('', views.main, name='main'), # root URL mapped to main views
+    path('members/', views.members, name='members'), # URL memebers mapped to members views
+    path('members/details/<int:id>', views.details, name='details'), # URL mapped to details view containing the details of the specific ID passed in the parameter
 ]
