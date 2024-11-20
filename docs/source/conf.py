15eb238e69c320ef9e1af381b8f896b2b240700f
Greg1400 <herzig.gregoire@gmail.com>
2024-11-20 09:57:30 +0100

    Installation of Sphinx documentation + update requirements.txt and .gitignore

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_bootstrap_theme
+import sphinxcontrib
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../')) # Adds project root of python path
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_tennis_club/settings.py'
+
+project = 'My Tennis Club'
+copyright = '2024, Herzig Gregoire'
+author = 'Herzig Gregoire'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinxcontrib.django',
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_static_path = ['_static']
